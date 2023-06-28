98515f0f64713089a37708025b2e9604030220e8
Timothy Karani <kartimothy@gmail.com>
2023-06-28 17:47:55 +0300

    Added postgres

--- chapter_6/auction_umbrella/apps/auction/lib/auction.ex
+++ chapter_6/auction_umbrella/apps/auction/lib/auction.ex
@@ -1,7 +1,7 @@
 defmodule Auction do
   alias Auction.{FakeRepo, Item}
 
-  @repo FakeRepo
+  @repo Auction.Repo
 
   def list_items do
     @repo.all(Item)
--- chapter_6/auction_umbrella/config/config.exs
+++ chapter_6/auction_umbrella/config/config.exs
@@ -59,6 +59,15 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :auction, ecto_repos: [Auction.Repo]
+
+config :auction, Auction.Repo,
+  username: "test_user",
+  password: "test_pass",
+  database: "auction",
+  hostname: "localhost",
+  port: "5432"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
