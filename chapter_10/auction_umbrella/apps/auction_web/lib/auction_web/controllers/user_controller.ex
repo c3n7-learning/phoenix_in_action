dc7bc5db548a598f965e3751be02a1f16b74491d
Timothy Karani <kartimothy@gmail.com>
2023-07-01 16:05:08 +0300

    Can restrict access

--- chapter_10/auction_umbrella/apps/auction_web/lib/auction_web/controllers/user_controller.ex
+++ chapter_10/auction_umbrella/apps/auction_web/lib/auction_web/controllers/user_controller.ex
@@ -1,5 +1,6 @@
 defmodule AuctionWeb.UserController do
   use AuctionWeb, :controller
+  plug :prevent_unauthorized_access when action in [:show]
 
   def show(conn, %{"id" => id}) do
     user = Auction.get_user(id)
@@ -15,6 +16,24 @@ defmodule AuctionWeb.UserController do
     case Auction.insert_user(user_params) do
       {:ok, user} -> redirect(conn, to: ~p"/users/#{user.id}")
       {:error, user} -> render(conn, :new, user: user)
+    end
+  end
+
+  defp prevent_unauthorized_access(conn, _opts) do
+    current_user = Map.get(conn.assigns, :current_user)
+
+    requested_user_id =
+      conn.params
+      |> Map.get("id")
+      |> String.to_integer()
+
+    if !current_user || current_user.id != requested_user_id do
+      conn
+      |> put_flash(:error, "Nice try, friend. That's not a page for you.")
+      |> redirect(to: ~p"/items")
+      |> halt()
+    else
+      conn
     end
   end
 end
