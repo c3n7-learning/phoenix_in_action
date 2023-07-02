234844f747b25cd3484ec1e8d6452b825e4834bc
Timothy Karani <kartimothy@gmail.com>
2023-07-02 20:36:02 +0300

    Begun work on chapter 14

--- chapter_13/auction_umbrella/config/test.exs
+++ chapter_13/auction_umbrella/config/test.exs
@@ -6,3 +6,11 @@ config :auction_web, AuctionWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
   secret_key_base: "DMbs8y0MQLhs3+1/En+LFMsJKhTaF+WwiJL5aI2EDS1FpaVzhtg2DvlqWaFxo9uU",
   server: false
+
+config :auction, Auction.Repo,
+  username: "test_user",
+  password: "test_pass",
+  database: "auction_test",
+  hostname: "localhost",
+  port: "5432"
+  pool: Ecto.Adapters.SQL.Sandbox
