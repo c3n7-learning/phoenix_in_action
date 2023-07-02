1e80695bef47fb475b7b97d98cb72c5e1db8be58
Timothy Karani <kartimothy@gmail.com>
2023-07-02 03:23:20 +0300

    Showing item bids

--- chapter_10/auction_umbrella/apps/auction_web/lib/auction_web/controllers/item_html.ex
+++ chapter_10/auction_umbrella/apps/auction_web/lib/auction_web/controllers/item_html.ex
@@ -2,4 +2,12 @@ defmodule AuctionWeb.ItemHTML do
   use AuctionWeb, :html
 
   embed_templates("item_html/*")
+
+  def integer_to_currency(cents) do
+    dollars_and_cents =
+      cents
+      |> Decimal.div(100)
+      |> Decimal.round(2)
+    "KES #{dollars_and_cents}"
+  end
 end
